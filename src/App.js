229f9167abda9a62c1fd6f39c19ab7be750b3077
Chili404 <kamalchill1278@gmail.com>
2023-01-03 17:23:32 -0500

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
-
+import {NavBar} from './Components/navbar';
+import 'bootstrap/dist/css/bootstrap.min.css'
 function App() {
   return (
     <div className="App">
+      <NavBar />
       <h1> Yo</h1>
     </div>
   );
